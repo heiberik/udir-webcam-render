55ce0c062cc92ed576a1f46030c79e797c7c9d11
Henrik Heiberg <heiberik@hotmail.com>
2023-01-05 10:56:43 +0100

    changed word list

--- client/src/util/nameHash.js
+++ client/src/util/nameHash.js
@@ -1,4 +1,4 @@
-import { names } from "./names"
+import { words } from "./words"
 
 function hashCode(s) {
     for (var i = 0, h = 0; i < s.length; i++)
@@ -6,9 +6,33 @@ function hashCode(s) {
     return h;
 }
 
-const getNameHash = (id) => {
-    let number = Math.abs(hashCode(id))
-    return names[number % names.length]
+function getNameHash(id) {
+
+    const firstIDPart = id.substring(0, id.length / 2)
+    const secondIDPart = id.substring(id.length / 2, id.length)
+
+    const oddIndex = getOddNumber(Math.abs(hashCode(firstIDPart)))
+    const evenIndex = getEvenNumber(Math.abs(hashCode(secondIDPart)))
+
+    const adjective = words[evenIndex % words.length]
+    const noun = words[oddIndex % words.length]
+
+    const name = adjective + " " + noun
+    return name
+}
+
+function getOddNumber(number) {
+    if (isOdd(number)) return number
+    else return number + 1
+}
+
+function getEvenNumber(number) {
+    if (!isOdd(number)) return number
+    else return number + 1
+}
+
+function isOdd(number) {
+    return number % 2
 }
 export {
     getNameHash
